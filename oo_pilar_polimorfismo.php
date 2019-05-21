451a59b4be322462bd8b935af45dc0ecf036c720
William <informatica01@COOPER.local>
2019-05-21 08:26:43 -0300

    Pilar do polimorfismo

---
+++ oo_pilar_polimorfismo.php
@@ -0,0 +1,66 @@
+<?php	
+	class veiculo{
+		public $placa=null;
+		public $cor=null;
+
+		function acelerar(){
+			echo 'Acelerar';
+		}
+
+		function freiar(){
+			echo 'Freiar';
+		}
+
+		function trocarMarcha(){
+			echo 'Desengatar embreagem com o pé e engatar marcha com a mão';
+		}
+	}
+
+	class Carro extends veiculo{
+
+		function __construct($placa, $cor){
+			$this->placa=$placa;
+			$this->cor=$cor;
+		}
+
+		public $teto_solar=true;
+
+		function abrirTetoSolar(){
+			echo 'Abrir teto solar';
+		}
+		function alterarPosicaoVolante(){
+			echo 'Alterar posição volante';
+		}
+	}
+
+	class Moto extends veiculo{
+
+		function __construct($placa, $cor){
+			$this->placa=$placa;
+			$this->cor=$cor;
+		}
+
+		public $contraPesoGuidao=true;
+
+		function empinar(){
+			echo 'Empinar';
+		}
+
+		function trocarMarcha(){
+			echo 'Desengatar embreagem com a mão e trocar de marcha com o pé';
+		}
+	}
+
+
+	$carro=new Carro('EIJ7766','Prata');
+	$carro->trocarMarcha();
+	
+	echo '<hr>';
+
+	$moto=new Moto('kkk123','Azul');
+	$moto->trocarMarcha();
+
+
+
+
+?>
